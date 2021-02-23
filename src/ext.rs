77ae207108cb83ea4175385cf4c3731cb67c024d
742362144 <742362144@qq.com>
2021-02-23 22:17:17 +0800

    fix bugs

--- src/ext.rs
+++ src/ext.rs
@@ -64,48 +64,48 @@ pub fn khop(policy: Arc<Mutex<Policy>>) -> Pin<Box<Generator<Yield=u64, Return=u
     })
 }
 
-pub fn rg(policy: Arc<Mutex<Policy>>) -> Pin<Box<Generator<Yield=u64, Return=u64> + 'static>> {
-    let mut p = policy.clone();
-    Box::pin(move || {
-        let (X, y) = iris::load_data();
-
-        let num_splits = 10;
-        let num_epochs = 5;
-
-        let mut accuracy = 0.0;
-
-        for (train_idx, test_idx) in CrossValidation::new(X.rows(), num_splits) {
-
-            let X_train = X.get_rows(&train_idx);
-            let y_train = y.get_rows(&train_idx);
-            let X_test = X.get_rows(&test_idx);
-            let y_test = y.get_rows(&test_idx);
-
-            let mut model = Hyperparameters::new(X.cols())
-                .learning_rate(0.5)
-                .l2_penalty(0.0)
-                .l1_penalty(0.0)
-                .one_vs_rest();
-
-            for _ in 0..num_epochs {
-                model.fit(&X_train, &y_train).unwrap();
-            }
-
-            let prediction = model.predict(&X_test).unwrap();
-            accuracy += accuracy_score(&y_test, &prediction);
-        }
-
-        accuracy /= num_splits as f32;
-
-        1111
-    })
-
-
-
-
-
-
-}
+// pub fn rg(policy: Arc<Mutex<Policy>>) -> Pin<Box<Generator<Yield=u64, Return=u64> + 'static>> {
+//     let mut p = policy.clone();
+//     Box::pin(move || {
+//         let (X, y) = iris::load_data();
+//
+//         let num_splits = 10;
+//         let num_epochs = 5;
+//
+//         let mut accuracy = 0.0;
+//
+//         for (train_idx, test_idx) in CrossValidation::new(X.rows(), num_splits) {
+//
+//             let X_train = X.get_rows(&train_idx);
+//             let y_train = y.get_rows(&train_idx);
+//             let X_test = X.get_rows(&test_idx);
+//             let y_test = y.get_rows(&test_idx);
+//
+//             let mut model = Hyperparameters::new(X.cols())
+//                 .learning_rate(0.5)
+//                 .l2_penalty(0.0)
+//                 .l1_penalty(0.0)
+//                 .one_vs_rest();
+//
+//             for _ in 0..num_epochs {
+//                 model.fit(&X_train, &y_train).unwrap();
+//             }
+//
+//             let prediction = model.predict(&X_test).unwrap();
+//             accuracy += accuracy_score(&y_test, &prediction);
+//         }
+//
+//         accuracy /= num_splits as f32;
+//
+//         1111
+//     })
+//
+//
+//
+//
+//
+//
+// }
 
 
 // pub fn init(policy: Arc<Mutex<Policy>>) -> Pin<Box<Generator<Yield=u64, Return=u64> + 'static>> {
